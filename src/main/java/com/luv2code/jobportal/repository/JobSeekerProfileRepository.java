377e99bc9aaa5876b13f6aa395c0e35ad052d5fe
Wida <mymacbook@MacBook-Pro.local>
2025-11-16 22:49:49 -0500

    Create JobSeekerRepository Interface

---
+++ src/main/java/com/luv2code/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.jobportal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.jobportal.entity.JobSeekerProfile;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer>{
+
+}
